5516bee46e72b5e1d98767b4bdd67e763db531fe
Ayanava De <ayanavade01@gmail.com>
2024-08-12 02:30:45 +0530

    [feat] Battery topbar widget popup
    Simple popup for displaying the time left to (dis)charge.

--- .config/awesome/ui/widgets/battery.lua
+++ .config/awesome/ui/widgets/battery.lua
@@ -1,3 +1,4 @@
+local awful = require("awful")
 local gshape = require("gears.shape")
 local wibox = require("wibox")
 
@@ -21,8 +22,46 @@ local bar_wgt = wibox.widget({
 	}
 })
 
+local popup_level = wibox.widget.textbox("00")
+local popup_waiting = wibox.widget.textbox("00:00:00")
+
+local battery_popup = awful.popup({
+	widget = {
+		widget = wibox.container.background,
+		bg = "#268f28",
+		{
+			widget = wibox.container.margin,
+			margins = 10,
+			{
+				layout = wibox.layout.fixed.horizontal,
+				spacing = 20,
+				popup_level, popup_waiting,
+			}
+		}
+	},
+	shape = function(cr, w, h) gshape.rounded_rect(cr, w, h, 5) end,
+	placement = { },
+	ontop = true,
+	visible = false
+})
+
+bar_wgt:buttons(
+	awful.button({ }, 1,
+	function()
+		awful.placement.next_to(battery_popup, {
+			preferred_positions = { "bottom" },
+			preferred_anchors = { "middle" },
+			mode = "cursor_inside",
+			offset = { y = 5 },
+		})
+		battery_popup.visible = not battery_popup.visible
+	end)
+)
+
 server:sync(function(mode, level, charging)
 	text_label.text = level.."%"
+	popup_level.text = level.."%"
+	popup_waiting.text = server.waiting
 end)
 
 return bar_wgt
