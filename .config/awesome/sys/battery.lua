d40b4d8d7a4f08031f2bf9eb8cbdb04b7af89fab
Ayanava De <ayanavade01@gmail.com>
2024-08-13 01:06:51 +0530

    [feat] No timer for battery
    The battery manager is now updated from an infinite loop using
    `awful.spawn.with_line_callback`.

--- .config/awesome/sys/battery.lua
+++ .config/awesome/sys/battery.lua
@@ -1,5 +1,4 @@
 local aspawn = require("awful.spawn")
-local gtimer = require("gears.timer")
 local gobject = require("gears.object")
 
 local battery = {
@@ -14,22 +13,39 @@ battery = gobject({ class = battery })
 
 function battery:update()
 	aspawn.easy_async("acpi -bi", function(out)
-		local level, waiting = out:match("(%d?%d?%d)%%, (%d%d:%d%d:%d%d)")
-		level = tonumber(level)
-		if level ~= self.level then
-			self.level = level
-			self.waiting = waiting
-			self:emit_signal("battery::update")
-		end
-		self.health = out:match("mAh = (%d?%d?%d)%%")
+		self.health = tonumber(out:match("mAh = (%d?%d?%d)%%"))
 	end)
+	aspawn.with_line_callback(
+		[[bash -c "
+			while true; do acpi -b | sed 's/Battery [0-9]\+:\s\+//'; sleep 10; done
+		"]],
+		{
+			stdout = function(out)
+				self.charging = out:sub(1, 1)
+				local level = tonumber(out:match("(%d?%d?%d)%%"))
+				local waiting = out:match("(%d%d:%d%d:%d%d)")
+				if level ~= self.level then
+					self.level = level
+					if not waiting then
+						self.waiting = "--:--:--"
+					else
+						self.waiting = waiting
+					end
+					self:emit_signal("battery::update")
+				end
+			end,
+			stderr = function(out)
+				local naughty = require("naughty")
+				naughty.notify({
+					title = "Oops! Error in sys.battery!",
+					text = out,
+					preset = naughty.config.presets.critical,
+				})
+			end,
+		}
+	)
 end
 
-battery.timer = gtimer({
-	timeout = 10,
-	call_now = false,
-	autostart = true,
-	callback = function() battery:update() end,
-})
+battery:update()
 
 return battery
