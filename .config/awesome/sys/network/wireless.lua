e59279e004088f60e8ef7a81397e84e0e7120464
Ayanava De <ayanavade01@gmail.com>
2024-09-02 15:54:08 +0530

    [wm][fix] Wireless device object need not be exported

--- .config/awesome/sys/network/wireless.lua
+++ .config/awesome/sys/network/wireless.lua
@@ -1,7 +1,8 @@
 local naughty = require("naughty")
 local proxy = require("dbus_proxy")
 
-local enums = require(... .. ".enums")
+local curr_dir = (...):match("(.-)[^%.]+$")
+local enums = require(curr_dir..".enums")
 
 local NM_NAME  = "org.freedesktop.NetworkManager"
 local NM_PATH  = "/org/freedesktop/NetworkManager"
@@ -12,6 +13,23 @@ local nm_server = proxy.Proxy:new({
 	name = NM_NAME,
 	path = NM_PATH,
 	interface = NM_IFACE
+})
+-- It is "wlp3s0" for me. I am aware it maybe different for other systems but I
+-- do not know how to check for that yet.
+local wireless_path = nm_server:GetDeviceByIpIface("wlp3s0")
+if not wireless_path then
+	naughty.notify({
+		title = "Networking",
+		text = "Could not find wireless device \"wlp3s0\". Is it connected?",
+		preset = naughty.config.presets.critical
+	})
+end
+naughty.notify({ title = "Networking", text = "Wireless dev: "..wireless_path, timeout = 0 })
+local wireless = proxy.Proxy:new({
+	bus = proxy.Bus.SYSTEM,
+	name = NM_NAME,
+	path = wireless_path,
+	interface = NM_IFACE..".Device"
 })
 
 local api = { }
@@ -96,7 +114,7 @@ setmetatable(api.Device, {
 			k == "HwAddress" or
 			k == "Ports"
 		then
-			return api.device:Get(NM_IFACE..".Device", k)
+			return wireless:Get(NM_IFACE..".Device", k)
 		end
 		-- Properties implemented from
 		-- "org.freedesktop.NetworkManager.Device.Wireless"
@@ -110,27 +128,9 @@ setmetatable(api.Device, {
 			k == "WirelessCapabilities" or
 			k == "LastScan"
 		then
-			return api.device:Get(NM_IFACE..".Device.Wireless", k)
+			return wireless:Get(NM_IFACE..".Device.Wireless", k)
 		end
 	end
-})
-
--- It is "wlp3s0" for me. I am aware it maybe different for other systems but I
--- do not know how to check for that yet.
-local wireless_path = nm_server:GetDeviceByIpIface("wlp3s0")
-if not wireless_path then
-	naughty.notify({
-		title = "Networking",
-		text = "Could not find wireless device \"wlp3s0\". Is it connected?",
-		preset = naughty.config.presets.critical
-	})
-end
-naughty.notify({ title = "Networking", text = "Wireless dev: "..wireless_path, timeout = 0 })
-api.device = proxy.Proxy:new({
-	bus = proxy.Bus.SYSTEM,
-	name = NM_NAME,
-	path = wireless_path,
-	interface = NM_IFACE..".Device"
 })
 
 function api.Get_ActiveAccessPoint()
@@ -168,7 +168,7 @@ local function refresh_state()
 end
 refresh_state()
 
-api.device:connect_signal(function (self, new_state, old_state, state_reason)
+wireless:connect_signal(function (self, new_state, old_state, state_reason)
 	naughty.notify({
 		title = "Networking signal",
 		text = "State! New: "..new_state..", Old: "..old_state..", Reason: "..state_reason
