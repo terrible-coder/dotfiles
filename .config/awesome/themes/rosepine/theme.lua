2845990d4b87256211a8f271fee04c1c4717b8f4
Ayanava De <ayanavade01@gmail.com>
2024-09-05 18:43:43 +0530

    [wm][theme] Rosepine colorscheme values added

--- .config/awesome/themes/rosepine/theme.lua
+++ .config/awesome/themes/rosepine/theme.lua
@@ -19,22 +19,23 @@ theme.fonts = {
 }
 theme.font = theme.fonts.sans..10
 
-theme.bg_normal     = "#222222"
-theme.bg_focus      = "#535d6c"
-theme.bg_urgent     = "#ff0000"
-theme.bg_minimize   = "#444444"
+theme.colors = require("themes.rosepine.colors").rosepine.main
+theme.bg_normal     = theme.colors.base
+theme.bg_focus      = theme.colors.pine
+theme.bg_urgent     = theme.colors.love
+theme.bg_minimize   = theme.colors.muted
 theme.bg_systray    = theme.bg_normal
 
-theme.fg_normal     = "#aaaaaa"
-theme.fg_focus      = "#ffffff"
-theme.fg_urgent     = "#ffffff"
+theme.fg_normal     = theme.colors.text
+theme.fg_focus      = theme.colors.base
+theme.fg_urgent     = theme.colors.hl_low
 theme.fg_minimize   = "#ffffff"
 
-theme.useless_gap   = dpi(0)
-theme.border_width  = dpi(1)
-theme.border_normal = "#000000"
-theme.border_focus  = "#535d6c"
-theme.border_marked = "#91231c"
+theme.useless_gap   = dpi(5)
+theme.border_width  = dpi(2)
+theme.border_normal = theme.colors.base.."00"
+theme.border_focus  = theme.colors.rose
+theme.border_marked = theme.colors.pine
 
 -- There are other variable sets
 -- overriding the default one when
