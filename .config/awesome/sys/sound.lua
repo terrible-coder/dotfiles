1af921c72d63754cb1158be06c163300bbc08ab5
Ayanava De <ayanavade01@gmail.com>
2024-09-07 20:37:15 +0530

    [wm][snd] Wrapper API for Core1 and Core1.Device
    Wrapping the PulseAudio Core1 DBus API in simple lua table syntax.
    Getters and setters carefully added.

--- .config/awesome/sys/sound.lua
+++ .config/awesome/sys/sound.lua
@@ -16,11 +16,15 @@ local connection = lgi.Gio.DBusConnection.new_for_address_sync(
 	pa_server.Address,
 	lgi.Gio.DBusConnectionFlags.AUTHENTICATION_CLIENT
 )
+
+local PA_PATH = "/org/pulseaudio/core1"
+local PA_IFACE = "org.PulseAudio.Core1"
+
 local core = proxy.Proxy:new({
 	bus = connection,
 	name = nil,
-	path = "/org/pulseaudio/core1",
-	interface = "org.PulseAudio.Core1"
+	path = PA_PATH,
+	interface = PA_IFACE
 })
 
 local api = { }
@@ -32,6 +36,114 @@ function api.object_from_path(path, interface)
 		path = path,
 		interface = interface,
 	})
+end
+
+setmetatable(api, {
+	__index = function (_, k)
+		if
+			k == "InterfaceRevision" or
+			k == "Name" or
+			k == "Version" or
+			k == "IsLocal" or
+			k == "Username" or
+			k == "Hostname" or
+			k == "DefaultChannels" or
+			k == "DefaultSampleFormat" or
+			k == "DefaultSampleRate" or
+			k == "Cards" or
+			k == "Sinks" or
+			k == "FallbackSink" or
+			k == "Sources" or
+			k == "FallbackSource" or
+			k == "PlaybackStreams" or
+			k == "RecordStreams" or
+			k == "Samples" or
+			k == "Modules" or
+			k == "Clients" or
+			k == "MyClient" or
+			k == "Extensions"
+		then
+			return core:Get(PA_IFACE, k)
+		end
+		local value = core[k]
+		if type(value) == "function" then
+			return function (...)
+				return value(core, ...)
+			end
+		end
+		return value
+	end,
+	__newindex = function (t, k, v)
+		if
+			k == "DefaultChannels" or
+			k == "DefaultSampleFormat" or
+			k == "DefaultSampleRate" or
+			k == "FallbackSink" or
+			k == "FallbackSource"
+		then
+			core:Set(PA_IFACE, k, v)
+		else
+			rawset(t, k, v)
+		end
+	end
+})
+
+function api.Device(path)
+	local DEV_IFACE = PA_IFACE .. ".Device"
+	local dev_obj = api.object_from_path(path, DEV_IFACE)
+	local dev = { }
+	setmetatable(dev, {
+		__index = function (_, k)
+			if
+				k == "Index" or
+				k == "Name" or
+				k == "Driver" or
+				k == "OwnerModule" or
+				k == "Card" or
+				k == "SampleFormat" or
+				k == "SampleRate" or
+				k == "Channels" or
+				k == "Volume" or
+				k == "HasFlatVolume" or
+				k == "HasConvertibleToDecibelVolume" or
+				k == "BaseVolume" or
+				k == "VolumeSteps" or
+				k == "Mute" or
+				k == "HasHardwareVolume" or
+				k == "HasHardwareMute" or
+				k == "ConfiguredLatency" or
+				k == "HasDynamicLatency" or
+				k == "Latency" or
+				k == "IsHardwareDevice" or
+				k == "IsNetworkDevice" or
+				k == "State" or
+				k == "Ports" or
+				k == "ActivePort" or
+				k == "PropertyList"
+			then
+				return dev_obj:Get(DEV_IFACE, k)
+			end
+			local value = dev_obj[k]
+			if type(value) == "function" then
+				return function (...)
+					return value(dev, ...)
+				end
+			end
+			return value
+		end,
+		__newindex = function (t, k, v)
+			if
+				k == "Volume" or
+				k == "Mute" or
+				k == "ActivePort"
+			then
+				dev_obj:Set(DEV_IFACE, k, v)
+			else
+				rawset(t, k, v)
+			end
+		end
+	})
+	return dev
 end
 
 function api:change(delta)
