864dcaff6ded22ba54ba5cc33fa7706126de120b
Ayanava De <ayanavade01@gmail.com>
2024-09-07 15:06:33 +0530

    [wm][snd] Connecting to PulseAudio via DBus
    Connecting to PulseAudio proxy directly without using
    stefano-m/lua-pulseaudio_dbus.

--- .config/awesome/sys/sound.lua
+++ .config/awesome/sys/sound.lua
@@ -1,5 +1,32 @@
 local aspawn = require("awful.spawn")
 local gobject = require("gears.object")
+local naughty = require("naughty")
+
+local lgi = require("lgi")
+local proxy = require("dbus_proxy")
+
+local pa_server = proxy.Proxy:new({
+	bus = proxy.Bus.SESSION,
+	name = "org.PulseAudio1",
+	path = "/org/pulseaudio/server_lookup1",
+	interface = "org.PulseAudio.ServerLookup1"
+})
+
+local connection = lgi.Gio.DBusConnection.new_for_address_sync(
+	pa_server.Address,
+	lgi.Gio.DBusConnectionFlags.AUTHENTICATION_CLIENT
+)
+local core = proxy.Proxy:new({
+	bus = connection,
+	name = nil,
+	path = "/org/pulseaudio/core1",
+	interface = "org.PulseAudio.Core1"
+})
+
+naughty.notify({
+	title = "sound server",
+	text = "Core: "..core.object_path
+})
 
 local sound = {
 	mute = false,
