8aee8870fe1b5ff134f571640c8e518b035c2965
Ayanava De <ayanavade01@gmail.com>
2024-09-09 13:13:03 +0530

    [wm][ui] Date and time widgets show together
    Ugh! No more changing your mind please! This is fine. Remove the weekday
    name if and when you add the calendar popup.

--- .config/awesome/ui/widgets/time.lua
+++ .config/awesome/ui/widgets/time.lua
@@ -3,20 +3,14 @@ local dpi = beautiful.xresources.apply_dpi
 local gshape = require("gears.shape")
 local wibox = require("wibox")
 
-local date_label = wibox.widget.textclock("%a %d %b, %Y")
-local date_icon = wibox.widget.textbox("󰃭")
-date_icon.font = beautiful.fonts.nerd..16
-
-local time_label = wibox.widget.textclock("%H:%M")
+local time_label = wibox.widget.textclock("%H:%M on (%a) %d %b, '%y")
 local time_icon = wibox.widget.textbox("󰥔")
 time_icon.font = beautiful.fonts.nerd..16
-
-local status = "time"
 
 local bar_wgt = wibox.widget({
 	layout = wibox.layout.fixed.horizontal,
 	{
-		widget = wibox.container.background, id = "icon_role",
+		widget = wibox.container.background,
 		shape = function(cr, w, h)
 			gshape.partially_rounded_rect(cr, w, h, true, false, false, true, dpi(2))
 		end,
@@ -28,7 +22,7 @@ local bar_wgt = wibox.widget({
 		}
 	},
 	{
-		widget = wibox.container.background, id = "text_role",
+		widget = wibox.container.background,
 		bg = beautiful.colors.hl_low,
 		shape = function(cr, w, h)
 			gshape.partially_rounded_rect(cr, w, h, false, true, true, false, dpi(2))
@@ -42,19 +36,5 @@ local bar_wgt = wibox.widget({
 		}
 	}
 })
-
-bar_wgt:connect_signal("button::press", function(_, _, _, button)
-	if button == 1 then
-		if status == "time" then
-			bar_wgt:get_children_by_id("icon_role")[1].children[1].widget = date_icon
-			bar_wgt:get_children_by_id("text_role")[1].children[1].widget = date_label
-			status = "date"
-		elseif status == "date" then
-			bar_wgt:get_children_by_id("icon_role")[1].children[1].widget = time_icon
-			bar_wgt:get_children_by_id("text_role")[1].children[1].widget = time_label
-			status = "time"
-		end
-	end
-end)
 
 return bar_wgt
