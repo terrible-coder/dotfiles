196bd22c94cc092476333eff83044e7e8d3e5c02
Ayanava De <ayanavade01@gmail.com>
2024-09-18 02:08:22 +0530

    [wm][ui][feat] Click events on bluetooth widget
    Clicking on bluetooth widget in topbar toggles the power on/off status.

--- .config/awesome/ui/widgets/bluetooth.lua
+++ .config/awesome/ui/widgets/bluetooth.lua
@@ -1,9 +1,22 @@
+local GLib = require("lgi").GLib
+local awful = require("awful")
 local gshape = require("gears.shape")
 local wibox = require("wibox")
 local beautiful = require("beautiful")
 local dpi = beautiful.xresources.apply_dpi
+local naughty = require("naughty")
 
-local wgt_icon = wibox.widget.textbox("󰂯")
+local bluetooth = require("sys.bluetooth")
+
+local icons = {
+	ON = "󰂯",
+	OFF = "󰂲",
+	CONNECTED = "󰂱",
+}
+
+local wgt_icon = wibox.widget.textbox(
+	bluetooth:Get("org.bluez.Adapter1", "Powered") and icons.ON or icons.OFF
+)
 wgt_icon.font = beautiful.fonts.nerd..12
 local wgt_label = wibox.widget.textbox("off")
 
@@ -36,5 +49,23 @@ local bar_widget = wibox.widget({
 		}
 	}
 })
+
+bar_widget:buttons(
+	awful.button({ }, 1, function()
+		bluetooth:Set("org.bluez.Adapter1", "Powered",
+			GLib.Variant.new("b", not bluetooth:Get("org.bluez.Adapter1", "Powered"))
+		)
+	end)
+)
+
+bluetooth:connect_signal(function(_, _, changed)
+	if changed.Powered ~= nil then
+		naughty.notify({
+			title = "Bluetooth",
+			text = "Switched " .. (changed.Powered and "on" or "off")
+		})
+		wgt_icon.text = changed.Powered and icons.ON or icons.OFF
+	end
+end, "PropertiesChanged")
 
 return bar_widget
