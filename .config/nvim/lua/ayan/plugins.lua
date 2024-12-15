15e177c622b64012a967818553092a9350a60079
Ayanava De <ayanavade01@gmail.com>
2024-12-15 23:13:49 +0530

    [nvim][chore] Refactor plugin setup code
    Pull in all the setup code for individual plugins into the main plugin
    file. The configuration need not happen _after_ everything is loaded.

--- .config/nvim/lua/ayan/plugins.lua
+++ .config/nvim/lua/ayan/plugins.lua
@@ -1,19 +1,47 @@
 return require("lazy").setup({
 	-- fuzzy file finder
 	{
-	  "nvim-telescope/telescope.nvim",
+		"nvim-telescope/telescope.nvim",
 		tag = "0.1.5",
-	  dependencies = {
-	  	"nvim-lua/plenary.nvim"
-	  }
+		dependencies = {
+			"nvim-lua/plenary.nvim"
+		},
+		opts = {
+			pickers = {
+				find_files = {
+					prompt_title = "Search files ("..vim.fn.getcwd()..")"
+				},
+				grep_string = {
+					prompt_title = "Search word ("..vim.fn.getcwd()..")"
+				},
+				live_grep = {
+					prompt_title = "Grep search ("..vim.fn.getcwd()..")"
+				}
+			},
+		}
 	},
 	-- colour scheme
-	-- { "Mofiqul/dracula.nvim" },
+	{ "Mofiqul/dracula.nvim" },
 	{ "rose-pine/neovim", name = "rose-pine" },
 	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = {
 			"nvim-tree/nvim-web-devicons"
+		},
+		opts = {
+			options = {
+				component_separators = "|",
+				section_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_c = {
+					{
+						"filename",
+						file_status = true, newfile_status = true,
+						path = 1,
+					},
+				},
+			}
 		}
 	},
 
@@ -23,6 +51,17 @@ return require("lazy").setup({
 		version = false,
 		build = ":TSUpdate",
 		cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+		opts = {
+			ensure_installed = { "bash", "c", "lua", "vim", },
+			sync_install = false,
+			auto_install = true,
+
+			highlight = {
+				enable = true,
+				-- disable = { "markdown" },
+				additional_vim_regex_highlighting = false,
+			},
+		}
 	},
 
 	-- LSP
@@ -48,11 +87,38 @@ return require("lazy").setup({
 	-- Work with Obsidian vaults directly from neovim
 	{
 		"epwalsh/obsidian.nvim",
+		enabled = false,
 		version = "*",
 		lazy = true,
 		ft = "markdown",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 		},
+		opts = {
+			workspaces = {
+				{ name = "Primary", path = "~/documents/notes/Primary" }
+			},
+			notes_subdir = "Zettelkasten",
+			daily_notes = {
+				folder = "Journal",
+				date_format = "%Y-%m-%d",
+			},
+			preferred_link_style = "wiki",
+			new_notes_location = "notes_subdir",
+			completion = {
+				nvim_cmp = true,
+				min_chars = 2,
+			},
+			disable_frontmatter = false,
+			ui = {
+				enable = true,
+				checkboxes = {
+					[" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+					["x"] = { char = "", hl_group = "ObsidianDone" },
+					[">"] = { char = "", hl_group = "ObsidianRightArrow" },
+					["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+				},
+			}
+		}
 	},
 })
