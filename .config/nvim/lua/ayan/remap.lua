23b5ebc03aa2a38b30cf34d3cfa2dabc7ef06665
Ayanava De <ayanavade01@gmail.com>
2025-02-10 13:59:04 +0530

    [nvim] Default split navigation
    Removing keybinds for navigating splits. Trying to get used to the
    proper vim way of doing things. Also, no more Obsidian keybinds
    necessary.

--- .config/nvim/lua/ayan/remap.lua
+++ .config/nvim/lua/ayan/remap.lua
@@ -25,15 +25,6 @@ vim.keymap.set("n", "<M-d>", "<PageDown>")
 vim.keymap.set("n", "<M-u>", "<PageUp>")
 vim.keymap.set("n", "G", "Gzz")
 
-------------------
----- Obsidian ----
-------------------
--- vim.keymap.set("n", "<leader>od", ":ObsidianToday ")
--- vim.keymap.set("n", "<leader>ob", ":ObsidianBacklinks<CR>")
--- vim.keymap.set("n", "<leader>os", ":ObsidianSearch ")
--- vim.keymap.set("n", "<leader>ot", ":ObsidianTags ")
--- vim.keymap.set("n", "<leader>oz", ":ObsidianNew<CR>")
-
 -------------------
 ---- Telescope ----
 -------------------
@@ -54,11 +45,6 @@ vim.keymap.set("n", "<C-Left>" , resize_jump.."<C-w><")
 vim.keymap.set("n", "<C-Right>", resize_jump.."<C-w>>")
 vim.keymap.set("n", "<C-Up>"   , resize_jump.."<C-w>+")
 vim.keymap.set("n", "<C-Down>" , resize_jump.."<C-w>-")
-vim.keymap.set("n", "<C-h>", "<C-w>h") -- shift focus to buffer on right
-vim.keymap.set("n", "<C-j>", "<C-w>j") -- shift focus to buffer on below
-vim.keymap.set("n", "<C-k>", "<C-w>k") -- shift focus to buffer on above
-vim.keymap.set("n", "<C-l>", "<C-w>l") -- shift focus to buffer on left
-vim.keymap.set("n", "<C-c>", "<C-w>c") -- close current buffer
 
 vim.keymap.set(
 	{ "i", "s" }, "<C-l>",
